7216d41a16dee2650d918c143afd77e98361678a
Pranav <pranavparasar321@gmail.com>
2024-06-01 09:00:24 +0530

    third commit

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,17 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { PostListContext } from "../store/Post-list-store";
 import Post from "./Post";
 import WelcomeMsg from "./WelcomeMsg";
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListContext);
-  const OnGetPostClick = () => {
+
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
-      .then((obj) => addInitialPosts(obj.posts));
-  };
+      .then((obj) => {
+        addInitialPosts(obj.posts);
+      });
+  }, []);
+
   return (
     <>
-      {postList.length === 0 && <WelcomeMsg OnGetPostClick={OnGetPostClick} />}
+      {postList.length === 0 && <WelcomeMsg />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- src/components/WelcomeMsg.jsx
+++ src/components/WelcomeMsg.jsx
@@ -3,9 +3,6 @@ const WelcomeMsg = ({ OnGetPostClick }) => {
     <>
       <center>
         <h1 className="wlcm">There are no posts</h1>
-        <button className="btn btn-primary mt-3" onClick={OnGetPostClick}>
-          Get Posts From Server
-        </button>
       </center>
     </>
   );
